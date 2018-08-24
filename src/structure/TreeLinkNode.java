435534096fc160947810dd4a1fc393aa1f33121a
qbosen <abosen@qq.com>
2018-08-24 17:31:29 +0800

    add structure TreeLinkNode

---
+++ src/structure/TreeLinkNode.java
@@ -0,0 +1,14 @@
+package structure;
+
+/**
+ * @author abosen
+ * @date 2018/8/24
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+
+    public TreeLinkNode(int x) {
+        val = x;
+    }
+}
