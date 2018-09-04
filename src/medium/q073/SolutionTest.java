bd906a7dee760f0a5819becf3ea469540bced4d3
qbosen <abosen@qq.com>
2018-09-04 16:58:34 +0800

    update 73

--- src/medium/q073/SolutionTest.java
+++ src/medium/q073/SolutionTest.java
@@ -1,50 +1,45 @@
 package medium.q073;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
-
 /**
- * @author abosen
- * @date 2018/8/1
+ * @author Abosen
+ * @date 2018/09/04
  */
 public class SolutionTest {
-
     @Test
-    public void test1() {
+    public void test() {
+        // simpleCase: [[1,1,1],[1,0,1],[1,1,1]]
         int[][] matrix = {
-                {0, 1, 2, 0},
-                {3, 4, 5, 2},
-                {1, 3, 1, 5}};
-
-
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}};
         new Solution().setZeroes(matrix);
+        int[][] expect = {
+                {1, 0, 1},
+                {0, 0, 0},
+                {1, 0, 1}};
 
-        System.out.println(Arrays.deepToString(matrix));
+        assertTrue(Arrays.deepEquals(expect, matrix));
     }
 
     @Test
     public void test2() {
         int[][] matrix = {
-                {1, 1, 1},
-                {1, 0, 1},
-                {1, 1, 1}};
+                {0, 1, 2, 0},
+                {3, 4, 5, 2},
+                {1, 3, 1, 5}};
 
         new Solution().setZeroes(matrix);
+        int[][] expect = {
+                {0, 0, 0, 0},
+                {0, 4, 5, 0},
+                {0, 3, 1, 0}};
 
-        System.out.println(Arrays.deepToString(matrix));
+        assertTrue(Arrays.deepEquals(expect, matrix));
     }
-
-    @Test
-    public void test3() {
-        int[][] matrix = {{1, 0, 3},};
-
-        new Solution().setZeroes(matrix);
-
-        System.out.println(Arrays.deepToString(matrix));
-    }
-
-
 }
